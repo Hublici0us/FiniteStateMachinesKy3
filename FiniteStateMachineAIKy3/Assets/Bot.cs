0eb1a26f44ffed9473b312af3dbd05e9877824c8
Jason Ky <744169@student.dusd.net>
2025-03-05 15:14:33 -0800

    ee

--- FiniteStateMachineAIKy3/Assets/Bot.cs
+++ FiniteStateMachineAIKy3/Assets/Bot.cs
@@ -88,9 +88,39 @@ public class Bot : MonoBehaviour
         Seek(chosenSpot);
     }
 
+    void CleverHide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+        Vector3 chosenDir = Vector3.zero;
+        GameObject chosenGO = World.Instance.GetHidingSpots()[0];
+
+        for (int i = 0; i < World.Instance.GetHidingSpots().Length; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 3;
+
+            if (Vector3.Distance(this.transform.position, hidePos) < dist)
+            {
+                chosenSpot = hidePos;
+                chosenDir = hideDir;
+                chosenGO = World.Instance.GetHidingSpots()[i];
+                dist = Vector3.Distance(this.transform.position, hidePos);
+            }
+        }
+
+        Collider hideCol = chosenGO.GetComponent<Collider>();
+        Ray backRay = new Ray(chosenSpot, -chosenDir.normalized);
+        RaycastHit info;
+        float distance = 100.0f;
+        hideCol.Raycast(backRay, out info, distance);
+
+        Seek(info.point + chosenDir.normalized * 5);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Hide();
+        CleverHide();
     }
 }
